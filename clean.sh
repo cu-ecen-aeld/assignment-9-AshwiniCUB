1f08dbb69692b44c11f09f2c55af7f990ab33e39
AshwiniCUB <ashwini.patil@colorado.edu>
2023-09-25 01:05:45 -0600

    clean.sh

--- clean.sh
+++ clean.sh
@@ -1,11 +1,4 @@
 #!/bin/sh
 
-cd `dirname $0`
-
-if [ ! -d buildroot/ ]
-then
-    echo "buildroot directory not found"
-else
-    cd buildroot
-    make distclean
-fi
+ cd buildroot
+ make distclean
