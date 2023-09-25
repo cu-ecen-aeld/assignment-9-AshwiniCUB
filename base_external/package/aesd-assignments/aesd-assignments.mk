bcc02a6cb0c8e9fac9f8bd7b2acb25efb6c4c961
AshwiniCUB <ashwini.patil@colorado.edu>
2023-09-25 01:05:31 -0600

    clean.sh

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,11 +6,11 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '771e56765a05f549f1c40ab0a086c796b2f6587f'
+AESD_ASSIGNMENTS_VERSION = 771e56765a05f549f1c40ab0a086c796b2f6587f
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-AshwiniCUB.git'
+AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-AshwiniCUB.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
